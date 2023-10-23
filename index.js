459ae517ffe8e7bea1d1280d47765dc197018430
Mazen Salah <101013388+MazenSalahb@users.noreply.github.com>
2023-10-23 21:22:13 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const mogoose = require("mongoose");
+const Article = require("./models/Article");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://mazen:ma01129977413@myfirstexpressjscluster.lv9nqgu.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch(() => {
+    console.log("Error With DB Connection");
+  });
+
+app.use(express.json());
+
+// mongodb+srv://mazen:ma01129977413@myfirstexpressjscluster.lv9nqgu.mongodb.net/?retryWrites=true&w=majority
+
+app.get("/", (req, res) => {
+  res.send("Welcome to express API");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/test", (req, res) => {
+  res.send("you visited test");
+});
+
+app.post("/addPost", (req, res) => {
+  res.send("Added new post");
+});
+
+app.get("/addNums/:num1/:num2", (req, res) => {
+  const sums = Number(req.params.num1) + Number(req.params.num2);
+  res.send(`result: ${sums}`);
+});
+
+app.get("/sayHello", (req, res) => {
+  const name = req.body.name;
+  res.send(`Hola ${name}`);
+});
+
+app.get("/allPosts", (req, res) => {
+  res.json({
+    id: 1,
+    title: "test post title 1",
+    body: "test body",
+  });
+});
+
+// ==================== Article ENDPOINTS ====================
+
+// Add Article
+app.post("/articles", (req, res) => {
+  const reqTitle = req.body.title.trim();
+  const reqBody = req.body.body.trim();
+  if (reqTitle == "" && reqBody == "") {
+    res.json({
+      status: "title and body are required",
+    });
+  } else {
+    const newArticle = new Article();
+    newArticle.title = reqTitle;
+    newArticle.body = reqBody;
+    newArticle.likes = 0;
+    newArticle
+      .save()
+      .then(() => {
+        res.json({
+          status: "success",
+          article: newArticle,
+        });
+      })
+      .catch(() => {
+        res.json({
+          status: "failed",
+        });
+      });
+  }
+});
+
+// Show Articles
+app.get("/articles", async (req, res) => {
+  const articles = await Article.find();
+  res.json(articles);
+});
+
+// Show Article by ID
+app.get("/articles/:articleID", (req, res) => {
+  const id = req.params.articleID;
+  const article = Article.findById(id)
+    .then(() => {
+      res.json(article);
+    })
+    .catch(() => {
+      res.json({
+        status: "error",
+      });
+    });
+});
+
+// Delete Article
+app.delete("/articles/:articleID", (req, res) => {
+  const id = req.params.articleID;
+  const article = Article.findByIdAndDelete(id)
+    .then(() => {
+      res.json({
+        status: "success",
+        article: article,
+      });
+    })
+    .catch(() => {
+      res.json({
+        status: "error",
+      });
+    });
+});
+
+app.listen(3000, () => {
+  console.log("listening in port 3000");
+});
---
+++ models/Article.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+const mogoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: String,
+  body: String,
+  likes: Number,
+});
+
+const Article = mogoose.model("Article", articleSchema);
+
+module.exports = Article;
